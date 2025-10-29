c50ce2b4dec2409268d7f22972b590b764019e77
jose-gonzalez7 <jgonzalezroman7@gmail.com>
2025-10-29 13:04:33 +0100

    Delete backend/src directory

--- backend/src/index.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
--- backend/src/routes/autenticacion.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { login } = require('../controllers/autenticacion.controller');
-
-router.post('/login', login);
-
-module.exports = router;
