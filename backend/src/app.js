e3dedc07ad3410b82a3d96f498972b7b97ef3434
jose-gonzalez7 <jgonzalezroman7@gmail.com>
2025-10-29 13:16:19 +0100

    Delete backend/src directory

--- backend/src/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const authRoutes = require('./routes/autenticacion.routes');
-
-app.use(express.json());
-app.use('/api', authRoutes);
-
-module.exports = app;
