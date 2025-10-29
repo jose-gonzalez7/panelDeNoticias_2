d973f194ef5cca00a3bd17802690b30ada93c20c
180abromotor@gmail.com <180abromotor@gmail.com>
2025-10-29 12:09:58 +0100

    guardas cambios locales antes del pull

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+/*module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
+}*/
+/** @type {import('postcss').ProcessOptions} */
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
 }
+
