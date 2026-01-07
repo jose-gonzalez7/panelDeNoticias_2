c65e03e6cf863bf9c45e5f48f1796eb69e90041f
jokunero <jokunero@gmail.com>
2026-01-07 22:10:21 +0100

    subida de : redireccion al login directo y modificador de busqueda en el panel de vision del profesor

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,13 @@
 'use client';
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const redireccion = useRouter();
+  redireccion.push("/login")
   
   return (
-    <div>hola</div>
+    <></>
   );
 }
--- frontend/app/vision/page.tsx
+++ frontend/app/vision/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useState, useEffect } from "react";
 import Publicacion from "@/components/Publicacion";
+import { DiVim } from "react-icons/di";
 
 type publicacion = {
   titulo: string;
@@ -49,8 +50,9 @@ async function mandarAapi(): Promise<publicacion[]> {
 
 export default function Home() {  
 
-  const [noticias, setnoticias] = useState<publicacion[]>([]);
-  const [modificador , setmodificador] = useState("nada")
+  const [publicaciones, setpublicaciones] = useState<publicacion[]>([]);
+  const [prioridad , setprioridad] = useState("nada")
+  const [buscador , setbuscador] = useState("")
 
   useEffect(() => {
     let mounted = true;
@@ -58,21 +60,44 @@ export default function Home() {
     const fetcher = async () => {
       const llamada = await mandarAapi();
       if (!mounted) return;
-      setnoticias(llamada);
+      setpublicaciones(llamada);
     };
 
     fetcher()
 
-    const id = setInterval(fetcher, 5000);
+    //const id = setInterval(fetcher, 5000);
 
     return () => {
       mounted = false;
-      clearInterval(id);
+      //clearInterval(id);
     };
   }, []);
 
-  function cambiarmod(mod : string) {
-    setmodificador(mod);
+  function resetearParametros() {
+    setbuscador("");
+    setprioridad("nada")
+  }
+
+  function cambiarbuscador(frase:string) {
+    setbuscador(frase)
+    console.log(frase)
+  }
+
+  function comprobarBusqueda(busqueda:string,titulo:string) : boolean{
+    let comprobar = false;
+    let titformateado = titulo
+    let busformateada = busqueda
+
+    // formatear palabra
+    titformateado = titformateado.toLowerCase()
+    busformateada = busformateada.toLowerCase()
+
+    // comporvar si el titulo contiene la busqueda
+    if (titformateado.includes(busformateada)) {
+      comprobar = true
+    }
+
+    return comprobar;
   }
 
   //!! aqui tendra que modificar el perez!!
@@ -81,33 +106,35 @@ export default function Home() {
       <div className="grid grid-cols-2 gap-4">
         {/* modificadores */}
         <div className="flex flex-col align-middle w-full gap-5">
-            <button onClick={()=>{setmodificador("nada")}} className="px-4 py-2 w-60 h-10 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">restablecer</button>
-            <button onClick={()=>{setmodificador("alta")}} className="px-4 py-2 w-60 h-10 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">prio alta</button>
-            <button onClick={()=>{setmodificador("media")}} className="px-4 py-2 w-60 h-10 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">prio media</button>
-            <button onClick={()=>{setmodificador("baja")}} className="px-4 py-2 w-60 h-10 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">prio baja</button>
+            {/* buscador */}
+            <div className="w-60">
+              <div className="relative">
+                <input onChange={(e)=>{cambiarbuscador(e.target.value)}} type="text" placeholder="Buscar..." className="w-full pl-10 pr-4 py-2 rounded-lg bg-white/60 backdrop-blur-sm border border-gray-200 text-gray-800 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400 transition-shadow shadow-sm"/>
+              </div>
+            </div>
+            {/* prioridad */}
+            <button onClick={()=>{resetearParametros()}} className="px-4 py-2 w-60 h-10 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">restablecer</button>
+            <button onClick={()=>{setprioridad("alta")}} className="px-4 py-2 w-60 h-10 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">prio alta</button>
+            <button onClick={()=>{setprioridad("media")}} className="px-4 py-2 w-60 h-10 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">prio media</button>
+            <button onClick={()=>{setprioridad("baja")}} className="px-4 py-2 w-60 h-10 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">prio baja</button>
         </div>
 
         {/* lista */}
         <div className="grid grid-cols-1 gap-4 bg-amber-300 rounded-lg p-6 text-center">
+          <p className="text-start"><strong className="bg-amber-100">Modificadores activos:</strong> <strong className="bg-green-500">{(prioridad != "nada")?"Prioridad":""}</strong>  <strong className="bg-green-500">{(buscador != "")?"Buscador":""}</strong></p>
           {
-            noticias.map((publicacion,indice) => (
-              (modificador == "nada")?
-                <div className="" key={indice}>
-                  <Publicacion pub={publicacion}></Publicacion>
-                </div>
-              :(modificador == "alta" && publicacion.prioridad == "alta")?
-                <div className="" key={indice}>
-                  <Publicacion pub={publicacion}></Publicacion>
-                </div>
-              :(modificador == "media" && publicacion.prioridad == "media")?
-                <div className="" key={indice}>
-                  <Publicacion pub={publicacion}></Publicacion>
-                </div>
-              :(modificador == "baja" && publicacion.prioridad == "baja")?
-                <div className="" key={indice}>
-                  <Publicacion pub={publicacion}></Publicacion>
-                </div>
-              :""
+            publicaciones.filter(publicacion => {
+              {/*filtrar solo las publicacioes que cumplen las siguientes condiciones*/}
+              const coincidePrioridad = prioridad === "nada" || prioridad === publicacion.prioridad
+              
+              const coincideBusqueda = buscador === "" || comprobarBusqueda(buscador, publicacion.titulo)
+
+              return coincidePrioridad && coincideBusqueda
+            })
+            .map((publicacion,indice) => (
+              <div key={indice}>
+                <Publicacion pub={publicacion} />
+              </div>
             ))
           }
         </div>
